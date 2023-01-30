a731ab379fbe4225c42391390bf985f9ed047cde
Tim Leaf <doctort144@gmail.com>
2023-01-29 21:15:07 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GW
+{
+	public static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			using (var game = new TestGame())
+				game.Run();
+		}
+	}
+}
---
+++ TestGame.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace GW
+{
+	public class TestGame : Game
+	{
+		public const int SCREEN_WIDTH = 1920;
+		public const int SCREEN_HEIGHT = 1080;
+
+		private GraphicsDeviceManager _graphics;
+		private SpriteBatch _spriteBatch;
+
+		//TODO: public SpriteFont basicFont;
+
+		//TODO: public Texture2D firstTexture;
+
+		//TODO: private World _world; 
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		public TestGame()
+		{
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+
+			_graphics.PreferredBackBufferWidth = SCREEN_WIDTH;
+			_graphics.PreferredBackBufferHeight = SCREEN_HEIGHT;
+
+			_graphics.HardwareModeSwitch = false;
+			_graphics.IsFullScreen = true;
+
+			IsFixedTimeStep = true;
+			TargetElapsedTime = TimeSpan.FromTicks(166667);
+
+			_graphics.SynchronizeWithVerticalRetrace = false;
+			_graphics.ApplyChanges();
+
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			// TODO: load fonts
+
+			// TODO: load sprites
+
+			// TODO: load music
+
+			// TODO: load sounds
+
+			// TODO: load the first world
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// game-specific content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+			// TODO: Unload any non ContentManager content here
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			// TODO: Add your update logic here
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			// TODO: Add your drawing code here
+
+			base.Draw(gameTime);
+		}
+	}
+}
